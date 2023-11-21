abe3f412a0a86b2527c017b31a7d38aa9d9f66d5
Ashesha <devcodedred@gmail.com>
2023-11-21 14:34:17 +0700

    Added readFile method, which reads the first line from a file. If the file does not exist, it creates it and returns an empty string

--- src/main/java/ru/ashesha/Main.java
+++ src/main/java/ru/ashesha/Main.java
@@ -1,7 +1,29 @@
 package ru.ashesha;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+    }
+
+
+    static String readFile(String name) {
+        try {
+            //noinspection ResultOfMethodCallIgnored
+            new File(name).createNewFile();
+        } catch (Throwable ignored) {
+        }
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(Files.newInputStream(Paths.get(name)), StandardCharsets.UTF_8))) {
+            return reader.readLine();
+        } catch (Throwable e) {
+
+            return "";
+        }
     }
 }
