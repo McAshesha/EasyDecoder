b232ac66d2802a8d437331cb58065c8df2c40cb3
Ashesha <mcashesha@mail.ru>
2023-11-22 12:05:15 +0700

    Added console argument for reading and writing the result
    Signed-off-by: Ashesha <mcashesha@mail.ru>

--- src/main/java/ru/ashesha/Main.java
+++ src/main/java/ru/ashesha/Main.java
@@ -11,60 +11,81 @@ import static java.lang.System.exit;
 
 public class Main {
 
-        public static void main(String[] args) {
-            boolean encode = false;
-            String input = "input.json", output = "output.json";
-            Decoder decoder = Decoder.getInstance();
+    public static void main(String[] args) {
+        boolean encode = false;
+        String input = "input", output = "output";
+        Decoder decoder = Decoder.getInstance();
 
-            for (String arg : args) {
-                if (arg.equalsIgnoreCase("-encode"))
-                    encode = true;
+        for (String arg : args) {
+            if (arg.equalsIgnoreCase("-encode"))
+                encode = true;
 
-                else if (arg.equalsIgnoreCase("-decode"))
-                    encode = false;
+            else if (arg.equalsIgnoreCase("-decode"))
+                encode = false;
 
-                else if (arg.startsWith("-lfm=")) {
-                    try {
-                        decoder.readLfm(Integer.parseInt(arg.substring(5)));
-                    } catch (Throwable e) {
-                        System.out.println("Invalid lfm format.");
-                        exit(-1);
-                    }
+            else if (arg.startsWith("-lfm=")) {
+                try {
+                    decoder.readLfm(Integer.parseInt(arg.substring(5)));
+                } catch (Throwable ignored) {
+                    System.out.println("Invalid lfm format.");
+                    exit(-1);
+                }
 
-                } else if (arg.startsWith("-in="))
-                    input = arg.substring(4) + ".json";
+            } else if (arg.startsWith("-in="))
+                input = arg.substring(4);
 
-                else if (arg.startsWith("-out="))
-                    output = arg.substring(5) + ".json";
-            }
-
-
-            try {
-                JSONArray array = new JSONArray(readFile(input));
-                if (encode)
-                    decoder.encodeArray(array);
-                else decoder.decodeArray(array);
-
-                write(output, array.toString());
-                System.out.println("Read the packet from " + input + " and display the result in " + output + ".");
-            } catch (Throwable e) {
-                System.out.println("Invalid packet format in " + input + ".");
-                exit(-1);
-            }
-
+            else if (arg.startsWith("-out="))
+                output = arg.substring(5);
         }
+
+        try {
+            JSONArray array = new JSONArray(read(input));
+            if (encode)
+                decoder.encodeArray(array);
+            else decoder.decodeArray(array);
+
+            write(output, array.toString());
+            System.out.println("Read the packet from " + input + " and display the result in " + output + ".");
+        } catch (Throwable ignored) {
+            System.out.println("Invalid packet format in " + input + ".");
+            exit(-1);
+        }
+    }
+
+    static String read(String name) {
+        if (name.equalsIgnoreCase("console")) {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8))) {
+                return reader.readLine();
+            } catch (Throwable ignored) {
+                return "";
+            }
+        }
+        return readFile(name + ".json");
+    }
+
+    static void write(String name, String text) {
+        if (name.equalsIgnoreCase("console")) {
+            try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8))) {
+                writer.write(text);
+                writer.flush();
+            } catch (Throwable ignored) {
+            }
+        } else writeInFile(name + ".json", text);
+    }
+
+
 
     static String readFile(String name) {
         createFile(name);
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(Files.newInputStream(Paths.get(name)), StandardCharsets.UTF_8))) {
             String result = reader.readLine();
             return result == null ? "" : toUTF8(result);
-        } catch (Throwable e) {
+        } catch (Throwable ignored) {
             return "";
         }
     }
 
-    static void write(String name, String text) {
+    static void writeInFile(String name, String text) {
         createFile(name);
         try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(Files.newOutputStream(Paths.get(name)), StandardCharsets.UTF_8))) {
             writer.write(toUTF8(text));
